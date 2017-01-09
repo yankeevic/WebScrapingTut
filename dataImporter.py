6fe4395296f34eb5f853063a846c3408c74554f2
Yankeevic <yankeevic@gmail.com>
2017-01-09 18:12:15 +0100

    structure the original code
    add a function , entry point and made scriptable

--- dataImporter.py
+++ dataImporter.py
@@ -1,8 +1,31 @@
+#!/usr/bin/python
+
+import sys
 import urllib
 
-u=urllib.urlopen('http://ctabustracker.com/bustime/map/getBusesForRoute.jsp?route=22')
-data = u.read()
-f = open('rt22.xml','wb')
-f.write(data)
-f.close()
+urlconstloc = 'http://ctabustracker.com/bustime/map/getBusesForRoute.jsp?route=22'
+sOutFile = 'rt22.xml'
 
+#open url location.. returns an xml file
+def ReadData(urlloc,outfile):
+	try:
+		u=urllib.urlopen(urlloc)
+		data = u.read()
+		fhand = open(outfile,'wb')
+		fhand.write(data)
+		fhand.close()
+		print(fhand)
+	except:
+		print error
+		print(fhand)
+def main():
+	argnum = len(sys.argv)
+	print 'Number of arguments:',argnum,'arguments.'
+	print 'Argument List:', str(sys.argv)
+	if argnum > 1:
+		ReadData(sys.argv[1],sOutFile)
+	else:
+		ReadData(urlconstloc,sOutFile)
+
+if __name__ =='__main__':
+	main()
