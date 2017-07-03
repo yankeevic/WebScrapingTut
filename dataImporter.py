0119df42bd440fa8f383511bdb2309b47bbab92a
VictorCases <yankeevic@gmail.com>
2017-07-03 11:31:40 +0200

    cleaned file of tabs. Made it py3 ready.

--- dataImporter.py
+++ dataImporter.py
@@ -3,29 +3,35 @@
 import sys
 import urllib
 
-urlconstloc = 'http://ctabustracker.com/bustime/map/getBusesForRoute.jsp?route=22'
+urlconstloc = 'http://ctabustracker.com/bustime/map/getBusesForRoute.jsp? \
+route=22'
 sOutFile = 'rt22.xml'
 
-#open url location.. returns an xml file
-def ReadData(urlloc,outfile):
-	try:
-		u=urllib.urlopen(urlloc)
-		data = u.read()
-		fhand = open(outfile,'wb')
-		fhand.write(data)
-		fhand.close()
-		print(fhand)
-	except:
-		print error
-		print(fhand)
-def main():
-	argnum = len(sys.argv)
-	print 'Number of arguments:',argnum,'arguments.'
-	print 'Argument List:', str(sys.argv)
-	if argnum > 1:
-		ReadData(sys.argv[1],sOutFile)
-	else:
-		ReadData(urlconstloc,sOutFile)
+# open url location.. returns an xml file
 
-if __name__ =='__main__':
-	main()
+
+def ReadData(urlloc, outfile):
+    try:
+        u = urllib.request.urlopen(urlloc)
+        data = u.read()
+        fhand = open(outfile, 'wb')
+        fhand.write(data)
+        fhand.close()
+        print(fhand)
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
+        print(fhand)
+        sys.exit(-1)  # raise
+
+
+def main():
+    argnum = len(sys.argv)
+    print('Number of arguments:', argnum, 'arguments.')
+    print('Argument List:', str(sys.argv))
+    if argnum > 1:
+        ReadData(sys.argv[1], sOutFile)
+    else:
+        ReadData(urlconstloc, sOutFile)
+
+if __name__ == '__main__':
+    main()
